604ecb44b473d2f870ec6f94ff91c0ee6a735af0
mjoliessem@gmail.com <mjoliessem@gmail.com>
2025-06-11 17:48:02 +0000

    COUNTRY8CODE DATA AND SQL LOAD : the 2 files must not have same names

--- models/COVID19_Epidemiological_Data/country_codes.sql
+++ models/COVID19_Epidemiological_Data/country_codes.sql
@@ -1,15 +1,15 @@
 with demographics as (
     select * from {{ source('COVID19_Epidemiological_Data', 'DEMOGRAPHICS') }}
 ),
-country_codes as (
-    select * from {{ ref('country_code') }}
+country_codess as (
+    select * from {{ ref('country_codess') }}
 ),
 final as (
     select
         demographics.total_population,
         country_codes.name
     from demographics
-    left join country_codes
-        on country_codes.code = demographics.ISO3166_1
+    left join country_codess
+        on country_codess.code = demographics.ISO3166_1
 )
 select * from final

7e03c3571626ed7b6fab32da3420de9441418926
mjoliessem@gmail.com <mjoliessem@gmail.com>
2025-06-11 20:53:35 +0000

    my first dbt model with auto_test

--- macros/auto_test.sql
+++ macros/auto_test.sql
@@ -1,4 +1,4 @@
 {% macro auto_test(number=1) %}
-    insert into DEMO_DB.PUBLIC.MY_FIRST_DBT_MODEL values({{ number }})
-    commit; -- Cette commande sert à valider l'insertion de la ligne sur la table 
+insert into DEMO_DB.PUBLIC.MY_FIRST_DBT_MODEL values({{ number }});
+commit; -- Cette commande sert à valider l'insertion de la ligne sur la table 
 {% endmacro %}
